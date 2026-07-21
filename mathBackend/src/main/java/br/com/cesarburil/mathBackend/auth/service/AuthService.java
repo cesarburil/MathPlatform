6a6bb345b9466d1e944322997b7296ad7aaf64e3
Cesar Buril <cesaareffects@gmail.com>
2026-07-21 18:49:14 -0300

    feat: criação da lógica de registro com senha criptografada

--- mathBackend/src/main/java/br/com/cesarburil/mathBackend/auth/service/AuthService.java
+++ mathBackend/src/main/java/br/com/cesarburil/mathBackend/auth/service/AuthService.java
@@ -1,15 +1,38 @@
 package br.com.cesarburil.mathBackend.auth.service;
 
 import br.com.cesarburil.mathBackend.auth.dto.UserDto;
+import br.com.cesarburil.mathBackend.auth.model.User;
+import br.com.cesarburil.mathBackend.auth.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
     public String generateToken(String username) {
         return "";
     }
 
     public String register(UserDto userDto) {
-        return "";
+
+        User aNewUser = User
+                .builder()
+                .username(userDto.getUsername())
+                .build();
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+        aNewUser.setPassword(encoder.encode(userDto.getPassword()));
+
+        User savedUser = userRepository.save(aNewUser);
+
+        return savedUser.getUsername();
     }
 }
