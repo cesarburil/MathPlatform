955644d38ddf9734c1a7e96fe02ff8ba4d32ca9e
Cesar Buril <cesaareffects@gmail.com>
2026-07-21 19:14:37 -0300

    feat: implementação inicial do filtro jwt

--- mathBackend/src/main/java/br/com/cesarburil/mathBackend/auth/config/JwtFilter.java
+++ mathBackend/src/main/java/br/com/cesarburil/mathBackend/auth/config/JwtFilter.java
@@ -1,9 +1,15 @@
 package br.com.cesarburil.mathBackend.auth.config;
 
+import br.com.cesarburil.mathBackend.auth.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,8 +17,54 @@ import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+
+    private UserService userService;
+
+    private ApplicationContext applicationContext;
+
+    public JwtFilter(UserService userService, ApplicationContext applicationContext) {
+        this.userService = userService;
+        this.applicationContext = applicationContext;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = "";
+        String token = "";
+        String username = "";
 
+        header = request.getHeader("Authorization");
+
+        if (header != "" && header.startsWith("Bearer ")) {
+
+            token = header.substring(7);
+
+            username = getUsernameFromToken(token);
+
+            if (username != "" && validateToken(token)) {
+
+                UserDetails user = userService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private boolean validateToken(String token) {
+        return false;
+    }
+
+    private String getUsernameFromToken(String token) {
+        return "";
     }
 }
