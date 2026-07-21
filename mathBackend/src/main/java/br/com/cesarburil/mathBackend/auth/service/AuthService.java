88919d210cb035643392d10822c0a560b3c48816
Cesar Buril <cesaareffects@gmail.com>
2026-07-21 18:56:12 -0300

    feat: estrutura dummy de geração de token

--- mathBackend/src/main/java/br/com/cesarburil/mathBackend/auth/service/AuthService.java
+++ mathBackend/src/main/java/br/com/cesarburil/mathBackend/auth/service/AuthService.java
@@ -3,8 +3,16 @@ package br.com.cesarburil.mathBackend.auth.service;
 import br.com.cesarburil.mathBackend.auth.dto.UserDto;
 import br.com.cesarburil.mathBackend.auth.model.User;
 import br.com.cesarburil.mathBackend.auth.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
 
 @Service
 public class AuthService {
@@ -17,8 +25,21 @@ public class AuthService {
 
 
     public String generateToken(String username) {
-        return "";
+        return Jwts
+                .builder()
+                .issuer(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5))
+                .claims(new HashMap<>())
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
+
+    private Key getKey() {
+        byte[] bytes = Decoders.BASE64.decode("");
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
 
     public String register(UserDto userDto) {
 
