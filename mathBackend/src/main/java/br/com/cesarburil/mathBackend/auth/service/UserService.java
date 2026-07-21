7b137224ef444eadd8f9928b8916c8513c558358
Cesar Buril <cesaareffects@gmail.com>
2026-07-21 18:37:20 -0300

    feat: estrutura User com entity, repository e service

--- mathBackend/src/main/java/br/com/cesarburil/mathBackend/auth/service/UserService.java
+++ mathBackend/src/main/java/br/com/cesarburil/mathBackend/auth/service/UserService.java
@@ -1,12 +1,22 @@
 package br.com.cesarburil.mathBackend.auth.service;
 
+import br.com.cesarburil.mathBackend.auth.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return userRepository.findByUsername(username);
     }
 }
